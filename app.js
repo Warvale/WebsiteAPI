92263b52d789fd5f20ee51b2c7e5b7fb812209b8
RONTheCookie <ronthecookie0101@gmail.com>
2017-09-28 22:35:44 +0300

    add GET /players

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 /*
 This is a really tiny micro-service.
 
-And, the one and only route: GET /news
+Routes:
+GET /news Returns some json which is the news.
+GET /players Returns a number of players on warvale. Updates every 30 seconds.
 */
 const express = require('express');
 const app = express();
 var news = require("./news.json");
+var players = -1; // Just as a default value before we ping the server.
 setInterval(()=>{
 news = reload("./news.json");
 }, 1000*60*5);
@@ -14,4 +17,20 @@ app.get('/news', (req, res)=>{
   res.json(news);
 });
 
+app.get("/players",(req,res)=>{
+res.json(players);
+});
+
 app.listen(3080);
+
+function ping(){
+require('minecraft-ping').ping_fe01fa({host:'warvale.net', port:25565}, function(err, response) {
+  if (err) console.error(err);
+  players = response.playersOnline;
+});
+
+}
+
+ping();
+
+setInterval(ping, 1000*30);
